867dbd557faa94f17064459097acf57c2c279fd6
MateoDedi <mateod@live.com>
2023-09-25 15:18:47 +0200

    added 06

--- Exo_06.js
+++ Exo_06.js
@@ -1,1 +1,15 @@
-function
+function animals(a, b, c) {
+   let chickens = a * 2;
+   let cows     = b * 4;
+   let pigs     = c * 4;
+
+   legs = chickens + cows + pigs;
+
+   return legs;
+}
+
+console.log(animals(2, 3, 5));
+
+console.log(animals(1, 2, 3));
+
+console.log(animals(5, 2, 8));
